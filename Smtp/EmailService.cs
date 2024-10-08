f074ebe02ade26bea5fa9d35f3ac114c44ba5099
Bryan Caballero <164503688+Bry2003@users.noreply.github.com>
2024-10-08 14:58:37 +0800

    Add files via upload

---
+++ Smtp/EmailService.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System.Net;
+using System.Net.Mail;
+
+public class EmailService
+{
+    private readonly SmtpClient _smtpClient;
+
+    public EmailService()
+    {
+        _smtpClient = new SmtpClient("sandbox.smtp.mailtrap.io", 2525)
+        {
+            Credentials = new NetworkCredential("aa02b39e963df5", "9bb27e8ad5e283"),
+            EnableSsl = true
+        };
+    }
+
+    public void SendEmail(string toEmail, string subject, string body)
+    {
+        var mailMessage = new MailMessage("from@example.com", toEmail)
+        {
+            Subject = subject,
+            Body = body,
+            IsBodyHtml = true
+        };
+
+        _smtpClient.Send(mailMessage);
+        Console.WriteLine("Email sent to: " + toEmail);
+    }
+}
